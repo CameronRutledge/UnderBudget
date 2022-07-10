08d91d144ffbf122ca3d67d85cc553ae07e1068a
CameronRutledge <crutledge2@live,maryville.edu>
2022-07-10 00:00:16 -0500

    13 Commits

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from Budget_Keeper import db, login_manager
+from Budget_Keeper_Site import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
